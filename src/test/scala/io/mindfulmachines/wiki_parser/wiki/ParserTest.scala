656a0c84c3edeb7233abd249d2d98a02a43b67a6
Ryo Ota <nwtgck@gmail.com>
2018-01-20 07:32:28 +0900

    [change] Recomment tests out

--- src/test/scala/io/mindfulmachines/wiki_parser/wiki/ParserTest.scala
+++ src/test/scala/io/mindfulmachines/wiki_parser/wiki/ParserTest.scala
@@ -13,34 +13,31 @@ class ParserTest extends FunSuite {
 //    assert(Parser.parseInternalLinks(pages.map(_._2)).count() == 7087)
 //  }
 
-  test("TODO"){
-    println(Resources.getResource("wikidump.xml").getPath)
-  }
-
+  
   test("testParsePages") {
     val rawpges = Parser.readWikiDump(Spark.sc, Resources.getResource("wikidump.xml").getPath)
     assert(Parser.parsePages(rawpges).count() == 85)
   }
-//
-//  test("testParseRedirects") {
-//    val rawpges = Parser.readWikiDump(Spark.sc, "file://" + Resources.getResource("wikidump.xml").getPath)
-//    val pages = Parser.parsePages(rawpges)
-//    val redirects = Parser.parseRedirects(pages.map(_._2))
-//    assert(redirects.count() == 67)
-//  }
-//
-//
-//  test("testReadWikiDump") {
-//    assert(Parser.readWikiDump(Spark.sc, "file://" + Resources.getResource("wikidump.xml").getPath).count() == 85)
-//  }
-//
-//  test("testReadPageCounts") {
-//    assert(Parser.readPageCounts(Spark.sc, "file://" + Resources.getResource("pagecounts").getPath).count() == 100)
-//  }
-//
-//
-//  test("testReadClickSteam") {
-//    assert(Parser.readClickSteam(Spark.sc, "file://" + Resources.getResource("clickstream.tsv").getPath).count() == 99)
-//  }
+
+  test("testParseRedirects") {
+    val rawpges = Parser.readWikiDump(Spark.sc, "file://" + Resources.getResource("wikidump.xml").getPath)
+    val pages = Parser.parsePages(rawpges)
+    val redirects = Parser.parseRedirects(pages.map(_._2))
+    assert(redirects.count() == 67)
+  }
+
+
+  test("testReadWikiDump") {
+    assert(Parser.readWikiDump(Spark.sc, "file://" + Resources.getResource("wikidump.xml").getPath).count() == 85)
+  }
+
+  test("testReadPageCounts") {
+    assert(Parser.readPageCounts(Spark.sc, "file://" + Resources.getResource("pagecounts").getPath).count() == 100)
+  }
+
+
+  test("testReadClickSteam") {
+    assert(Parser.readClickSteam(Spark.sc, "file://" + Resources.getResource("clickstream.tsv").getPath).count() == 99)
+  }
 
 }
